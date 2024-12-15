b3bade3e958a30fcc9cd567c3428f62060e66116
c3cc3 <gwisang.choi@gmail.com>
2024-12-16 00:22:31 +0900

    build.sh change

--- examples/build.sh
+++ examples/build.sh
@@ -1,3 +1,4 @@
 set -x
 javac -classpath ./../lib/FileQueueJNI.jar:.:./../lib/json-20210307.jar TestDeQXA.java
 javac -classpath ./../lib/FileQueueJNI.jar:.:./../lib/json-20210307.jar TestEnQ_loop.java
+javac -classpath ./../lib/FileQueueJNI.jar:.:./../lib/json-20210307.jar CoAgent.java
